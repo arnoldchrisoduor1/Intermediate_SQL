0ebebc944496753f5e125ebcd2c127e658cbafad
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-09-10 12:34:51 +0300

    having clasue

---
+++ Having_clause.sql
@@ -0,0 +1,7 @@
+SELECT JobTitle, AVG(Salary)
+FROM EmployeeDemographics
+JOIN EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING AVG(Salary) > 45000
+ORDER BY AVG(Salary)
