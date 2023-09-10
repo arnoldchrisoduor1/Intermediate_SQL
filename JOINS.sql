deacea86a9cba5bdd9a754e7ac9ca7128ae9809c
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-09-10 11:29:40 +0300

    sql joins

--- JOINS.sql
+++ JOINS.sql
@@ -40,3 +40,13 @@ FROM EmployeeDemographics
 	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
 WHERE JobTitle = 'Salesman'
 GROUP BY JobTitle
+
+/*New Data*/
+Insert into EmployeeDemographics VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL, 'Holly','Flax', NULL, 'Male'),
+(1013, 'Darryl', 'Philbin', NULL, 'Male')
+
+Insert into EmployeeSalary VALUES
+(1010, NULL, 47000),
+(NULL, 'Salesman', 43000)
