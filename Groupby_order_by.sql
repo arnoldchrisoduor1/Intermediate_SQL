96c895c173d45fda57d0e6366986592b73cd1cd5
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-09-10 10:44:06 +0300

    order by

--- Groupby_order_by.sql
+++ Groupby_order_by.sql
@@ -3,3 +3,11 @@ FROM EmployeeDemographics
 WHERE Age > 31
 GROUP BY Gender
 ORDER BY Gender DESC
+
+SELECT *
+FROM EmployeeDemographics
+ORDER BY Age, Gender
+
+SELECT *
+FROM EmployeeDemographics
+ORDER BY 4 DESC, 5 DESC
